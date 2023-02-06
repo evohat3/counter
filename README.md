26781ed57a831dd804bf6e346f0e18bf9b595707
evohat3 <118646438+evohat3@users.noreply.github.com>
2023-02-06 16:10:41 -0600

    Version 1.1

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # counter
+this is a counter application that can be used to add or subtract in 1, 5 or 10   number increments.
+
+## Getting Started
+
+you can view the the counter here:
+https://evohat3.github.io/counter
+
+
+## Built With
+
+Html, Css and Javascript
+**************************************************************
+## Updates
+**************************************************************
+### 1.1
+
+* added content to README file
+
+* updated the buttons to reflect (-,+) 10 buttons and (-,+) 5 buttons
+
+* added new styling to make the buttons look more like buttons
+
+* added visual hover elements to all the buttons
+
+![Screenshot 1.1](./Assets/1.1.png)
+
+
+**************************************************************
+### 1.0 
+
+* created Counter App with +1 -1 and a reset button
+
+![Screenshot 1.0](./assets/1.0.png)
+
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
 let count = 0;
 
+document.getElementById("decreaseBtn10").onclick = function(){
+    count -=10;
+    document.getElementById("countLabel").innerHTML = count;
+
+}
+document.getElementById("decreaseBtn5").onclick = function(){
+    count -=5;
+    document.getElementById("countLabel").innerHTML = count;
+
+}
 document.getElementById("decreaseBtn").onclick = function(){
     count -=1;
     document.getElementById("countLabel").innerHTML = count;
@@ -12,6 +22,16 @@ document.getElementById("resetBtn").onclick = function(){
 }
 document.getElementById("increaseBtn").onclick = function(){
     count +=1;
+    document.getElementById("countLabel").innerHTML = count;
+    
+}
+document.getElementById("increaseBtn5").onclick = function(){
+    count +=5;
+    document.getElementById("countLabel").innerHTML = count;
+    
+}
+document.getElementById("increaseBtn10").onclick = function(){
+    count +=10;
     document.getElementById("countLabel").innerHTML = count;
     
 }
